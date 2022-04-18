6b1f111b6ed01ba549f4e1f53d235bf4c851e553
Stepan Kislyakov <lotup@mail.ru>
2022-04-18 15:49:16 +0600

    new api features created

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,13 +1,11 @@
-const fs = require('fs')
+const fs = require('fs');
 const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const Tour = require(`./../../dev-data/data/tour5`)
-
 dotenv.config({ path: './config.env' });
-
+const mongoose = require('mongoose');
+const Tour = require(`./../../models/tourModel`);
 
 //mongoose для доступа к БД
-
+console.log(`lets start`);
 const DB = process.env.DB_URL.replace('<PASSWORD>', process.env.DB_PASS);
 
 mongoose
@@ -20,35 +18,32 @@ mongoose
     console.log('DB connection success');
   });
 // read files
-const toures = JSON.parse(fs.readFileSync(`${__dirname}/tours`, 'utf-8'))
+const toures = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 //IMPORT DATA
-const importData = async()=>{
-    try{
-await Tour.create(toures)
-console.log(`data successfully loaded!`);
-process.exit() //закончит приложение
-    }catch(err){
-        console.log(err);
-    }
-}
+const importData = async () => {
+  try {
+    await Tour.create(toures);
+    console.log(`data successfully loaded!`);
+    process.exit(); //закончит приложение
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 //DELETE ALL data from COLLECTION
-const deleteData = async () =>{
-    try{
-        await Tour.deleteMany()
-        console.log(`data successfully deleted!`);
-        process.exit() //закончит приложение
-            }catch(err){
-                console.log(err);
-            }
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log(`data successfully deleted!`);
+    process.exit(); //закончит приложение
+  } catch (err) {
+    console.log(err);
+  }
+};
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
 }
-if(process.argv[2] ==='--import'){
-    importData()
-    
-} else if (process.argv[2] ==='--delete'){
-    deleteData()
-
-}
-
 
 console.log(process.argv);
