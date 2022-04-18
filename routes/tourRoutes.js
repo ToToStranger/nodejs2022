bf5897173e50f80cb28b85947dee18a9b115f9cf
stepan kislyakov <lotup@mail.ru>
2022-04-18 17:45:44 +0600

    aggregator configuration

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -9,12 +9,15 @@ router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
+router.route('/tour-stats').get(tourController.getTourStats)
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan)
+
+
 router
   .route('/')
   .get(tourController.getAllTours)
   .post(tourController.createTour); // первая функция это middleware, вторая сам маршрут
 router
-  .route('/:id')
   .get(tourController.getTour)
   .patch(tourController.updateTour)
   .delete(tourController.deleteTour);
