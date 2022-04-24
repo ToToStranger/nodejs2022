70d2fadb35d1b117d0545219b7e82549b46af1db
Stepan Kislyakov <lotup@mail.ru>
2022-04-24 22:25:30 +0600

    error handling

--- app.js
+++ app.js
@@ -49,4 +49,27 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// default route
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `can't find ${req.originalUrl}`,
+  // });
+  const err = new Error(`can't find ${req.originalUrl}`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err); // если мы пихаем чтото в next то это точно ошибка
+});
+
+//middlewaare функция для ловли ошибок всего приложения
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
+
 module.exports = app;
